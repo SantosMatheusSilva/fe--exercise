e36e8a54ed5f02b00e1765d8e600c0b915bbe030
SantosMatheusSilva <mathsantos52@gmail.com>
2024-04-09 09:33:48 +0100

    userService done

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -3,6 +3,15 @@ import axios from "axios";
 //
 const API_URL = "http://localhost:3000/users";
 // Function to get all users
+export async function getAllUsers() {
+    try {
+        const response = await axios.get(`${API_URL}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        throw error;
+    }
+}
 
 // Function to fetch user data by user ID
 export async function getUser(userId) {
@@ -26,4 +35,13 @@ export async function updateUserPProfile(userId, updatedProfileData) {
     }
 }
 
-//
+// Function to delete user
+export async function deleteUser(userId) {
+    try {
+        const response = await axios.delete(`${API_URL}/${userId}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        throw error;
+    }
+}
